b1bb5a2969222240d8f9263cd22e4b777d1414d3
unknown <XX935@ccs.local>
2025-02-28 15:07:16 +0800

    Styling register page - not yet done

--- Subdivision/Controllers/HomeController.cs
+++ Subdivision/Controllers/HomeController.cs
@@ -26,6 +26,10 @@ namespace Subdivision.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
